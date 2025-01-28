d5971d49b7b35cfdb43f382c0ad13b7f876c81a6
Duong Tran <tranduong1988@gmail.com>
2025-01-28 11:39:14 +0700

    correct path

--- ArchLinux/install_theme.sh
+++ ArchLinux/install_theme.sh
@@ -3,10 +3,10 @@ sudo pacman -S --noconfirm ttf-roboto
 sudo pacman -S --noconfirm ttf-0xproto-nerd 
 
 mkdir -p $HOME/.local/share/icons
-git clone https://github.com/EliverLara/candy-icons.git $HOME/.local/share/icons
+git clone https://github.com/EliverLara/candy-icons.git $HOME/.local/share/icons/candy-icons
 
 mkdir -p $HOME/.local/share/themes
-git clone https://github.com/EliverLara/Nordic.git $HOME/.local/share/themes
+git clone https://github.com/EliverLara/Nordic.git $HOME/.local/share/themes/Nordic
 
 xfconf-query -c xsettings -p /Net/ThemeName -s "Nordic"
 xfconf-query -c xsettings -p /Net/IconThemeName -s "candy-icons"
@@ -15,3 +15,5 @@ xfconf-query -c xfwm4 -p /general/theme -s "Nordic"
 xfconf-query -c xsettings -p /Gtk/FontName -s "Roboto Regular 10"
 xfconf-query -c xfwm4 -p /general/title_font -s "Roboto Bold 9"
 xfconf-query -c xfce4-terminal -p /font-name -s "0xProto Nerd Font Regular 12"
+
+gtk-update-icon-cache-f -t $HOME/.local/icons/candy-icons
