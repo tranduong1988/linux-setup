186b6405c20792086fd6fba39ed7e2dbd087b393
Duong Tran <tranduong1988@gmail.com>
2025-04-06 13:23:37 +0700

    add -y

--- Debian/install_packages.sh
+++ Debian/install_packages.sh
@@ -40,7 +40,7 @@ curl https://pyenv.run | bash
 # Docker
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
