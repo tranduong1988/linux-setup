276e09813e1880a7ecdc90c77a06a47077543b87
Duong Tran <tranduong1988@gmail.com>
2025-04-09 21:50:31 +0700

    change icon theme and theme

--- ArchLinux/install_theme.sh
+++ ArchLinux/install_theme.sh
@@ -2,12 +2,12 @@
 sudo pacman -S --noconfirm ttf-roboto
 sudo pacman -S --noconfirm ttf-0xproto-nerd 
 
-paru -S --noconfirm candy-icons-git
-paru -S --noconfirm nordic-theme-git 
+paru -S --noconfirm qogir-icon-theme
+paru -S --noconfirm materia-gtk-theme 
 
-xfconf-query -c xsettings -p /Net/ThemeName -s "Nordic"
-xfconf-query -c xsettings -p /Net/IconThemeName -s "candy-icons"
-xfconf-query -c xfwm4 -p /general/theme -s "Nordic"
+xfconf-query -c xsettings -p /Net/ThemeName -s "Materia-dark"
+xfconf-query -c xsettings -p /Net/IconThemeName -s "Qogir-dark"
+xfconf-query -c xfwm4 -p /general/theme -s "Materia-dark"
 
 xfconf-query -c xsettings -p /Gtk/FontName -s "Roboto Regular 10"
 xfconf-query -c xfwm4 -p /general/title_font -s "Roboto Bold 9"
