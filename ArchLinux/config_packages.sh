3476c4f8b80985701bf6a6a339350acee6d79f17
Duong Tran <tranduong1988@gmail.com>
2025-04-25 20:51:13 +0700

    remove line for nvm in bashrc

--- ArchLinux/config_packages.sh
+++ ArchLinux/config_packages.sh
@@ -27,8 +27,7 @@ eval \"\$(goenv init -)\"" ~/.bashrc
 add_text_to_file "
 [ -z \"$NVM_DIR\" ] && export NVM_DIR=\"\$HOME/.nvm\"
 source /usr/share/nvm/nvm.sh
-source /usr/share/nvm/bash_completion
-source /usr/share/nvm/install-nvm-exec" ~/.bashrc
+source /usr/share/nvm/bash_completion" ~/.bashrc
 
 # starship
 add_text_to_file "
