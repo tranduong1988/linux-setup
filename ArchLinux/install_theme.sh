5810a12bebd17e9e60be12d8fde59e33cb51b6af
Duong Tran <tranduong1988@gmail.com>
2025-07-03 11:52:28 +0700

    added 'xfce4-panel -r'

--- ArchLinux/install_theme.sh
+++ ArchLinux/install_theme.sh
@@ -23,4 +23,3 @@ xfconf-query -c xfce4-terminal -p /background-darkness -s 0.75 --create -t doubl
 xfconf-query -c xfce4-terminal -p /misc-default-geometry -s "120x30" --create -t string
 
 cp xfce4-panel.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
-xfce4-panel -r
