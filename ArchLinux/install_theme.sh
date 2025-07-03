32a0d391cbbef69246a086cfaeba04a593c10e4f
Duong Tran <tranduong1988@gmail.com>
2025-07-03 11:18:53 +0700

    added --create -t string

--- ArchLinux/install_theme.sh
+++ ArchLinux/install_theme.sh
@@ -9,12 +9,12 @@ yay -S --noconfirm qogir-icon-theme
 # yay -S --noconfirm materia-gtk-theme
 yay -S --noconfirm orchis-theme
 
-xfconf-query -c xsettings -p /Net/ThemeName -s "Orchis-Dark-Compact"
-xfconf-query -c xsettings -p /Net/IconThemeName -s "Qogir-dark"
-xfconf-query -c xfwm4 -p /general/theme -s "Orchis-Dark-Compact"
+xfconf-query -c xsettings -p /Net/ThemeName -s "Orchis-Dark-Compact" --create -t string
+xfconf-query -c xsettings -p /Net/IconThemeName -s "Qogir-dark" --create -t string
+xfconf-query -c xfwm4 -p /general/theme -s "Orchis-Dark-Compact" --create -t string
 
-xfconf-query -c xsettings -p /Gtk/FontName -s "Inter Display Regular 10"
-xfconf-query -c xfwm4 -p /general/title_font -s "Inter Display Bold 9"
+xfconf-query -c xsettings -p /Gtk/FontName -s "Inter Display Regular 10" --create -t string
+xfconf-query -c xfwm4 -p /general/title_font -s "Inter Display Bold 9" --create -t string
 
 xfconf-query -c xfce4-terminal -p /font-name -s "0xProto Nerd Font Regular 12" --create -t string
 xfconf-query -c xfce4-terminal -p /misc-menubar-default -s false --create -t bool
