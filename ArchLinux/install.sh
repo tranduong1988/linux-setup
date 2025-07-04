4dfd0e6165e2a54eec2bd8c6e7b8e37d1ae2d99a
Duong Tran <tranduong1988@gmail.com>
2025-07-04 13:56:47 +0700

    remove set_config in install

--- ArchLinux/install.sh
+++ ArchLinux/install.sh
@@ -7,7 +7,7 @@ mkdir -p $HOME/.cache/$commandyay
 sudo mount -t tmpfs -o size=2G tmpfs /var/cache/pacman/pkg
 sudo mount -t tmpfs -o size=2G tmpfs $HOME/.cache/$commandyay
 
-./set_config.sh
+# ./set_config.sh
 ./install_theme.sh
 ./install_packages.sh
 ./config_packages.sh
