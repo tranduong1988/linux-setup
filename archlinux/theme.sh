cd36e7a9ae39cd2721b16e2963c57e302b5de697
Duong Tran <tranduong1988@gmail.com>
2025-07-10 09:34:02 +0700

    added Matcha theme + papirus icon for arch

--- archlinux/theme.sh
+++ archlinux/theme.sh
@@ -5,8 +5,8 @@ source utils.sh
 # Install AUR helper if not found
 AUR_HELPER=$(get_aur_helper)
 
-$AUR_HELPER -S --mflags --skipinteg --noconfirm --needed qogir-icon-theme qogir-gtk-theme
+$AUR_HELPER -S --mflags --skipinteg --noconfirm --needed papirus-icon-theme matcha-gtk-theme
 
-xfconf-query -c xsettings -p /Net/ThemeName -s "Qogir-Dark" --create -t string
-xfconf-query -c xsettings -p /Net/IconThemeName -s "Qogir-dark" --create -t string
-xfconf-query -c xfwm4 -p /general/theme -s "Qogir-Dark" --create -t string
+xfconf-query -c xsettings -p /Net/ThemeName -s "Matcha-dark-sea" --create -t string
+xfconf-query -c xsettings -p /Net/IconThemeName -s "Papirus-Dark" --create -t string
+xfconf-query -c xfwm4 -p /general/theme -s "Matcha-dark-sea" --create -t string
