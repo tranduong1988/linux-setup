67586941f26bec4289dd823239262b404f8eb32a
Duong Tran <tranduong1988@gmail.com>
2025-07-10 09:06:01 +0700

    change Qogir-Dark

--- archlinux/theme.sh
+++ archlinux/theme.sh
@@ -5,8 +5,8 @@ source utils.sh
 # Install AUR helper if not found
 AUR_HELPER=$(get_aur_helper)
 
-$AUR_HELPER -S --mflags --skipinteg --noconfirm --needed qogir-icon-theme orchis-theme
+$AUR_HELPER -S --mflags --skipinteg --noconfirm --needed qogir-icon-theme qogir-gtk-theme
 
-xfconf-query -c xsettings -p /Net/ThemeName -s "Orchis-Dark-Compact" --create -t string
+xfconf-query -c xsettings -p /Net/ThemeName -s "Qogir-Dark" --create -t string
 xfconf-query -c xsettings -p /Net/IconThemeName -s "Qogir-dark" --create -t string
-xfconf-query -c xfwm4 -p /general/theme -s "Orchis-Dark-Compact" --create -t string
+xfconf-query -c xfwm4 -p /general/theme -s "Qogir-Dark" --create -t string
