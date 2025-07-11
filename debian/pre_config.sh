de4a33bb88707e74633d64c54fdb3d9166de6b15
Duong Tran <tranduong1988@gmail.com>
2025-07-11 12:34:48 +0700

    added googlechrome cache

--- debian/pre_config.sh
+++ debian/pre_config.sh
@@ -15,6 +15,13 @@ echo 'Set greeter-hide-users and greeter-show-manual-login'
 find_and_replace "#greeter-hide-users=false" "greeter-hide-users=false" /etc/lightdm/lightdm.conf
 find_and_replace "#greeter-show-manual-login=false" "greeter-show-manual-login=false" /etc/lightdm/lightdm.conf
 
+# set cache for google-chrome
+echo 'set google-chrome cache to ram'
+current=$(pwd)
+cd ../ram-cache
+bash install.sh
+cd $current
+
 # set cache to ram
 add_text_to_file "Dir::Cache    /tmp/apt_cache;" /etc/apt/apt.conf
 add_text_to_file "d /tmp/apt_cache/archives/partial 0755 root root -" /etc/tmpfiles.d/apt_cache.conf
